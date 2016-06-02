72e1cddc16e8059ec42c3e54f37c7e5f6eb46e64
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-06-02 17:00:43 +0100

    tidtyig dimensions in soils file as well

--- convert_ancil.sh
+++ convert_ancil.sh
@@ -3,4 +3,8 @@ cd outputs
 ./../libs/conv2nc_ltol.tcl ../data/gc3_orca1_mask
 cd ..
 python addLatLon.py
-ncwa -a t outputs/qrparm.veg.nc -O outputs/qrparm.veg.nc
+
+ncwa -a t outputs/qrparm.veg.frac.nc -O outputs/qrparm.veg.frac.nc
+ncwa -a t outputs/qrparm.soil.nc -O outputs/qrparm.soil.nc
+ncwa -a surface outputs/qrparm.soil.nc -O outputs/qrparm.soil.nc
+
