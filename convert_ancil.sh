308b6f2112f067cbc5ef82236b220f996fbcff88
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-06-02 13:26:49 +0100

    removing extra dims from veg cover

--- convert_ancil.sh
+++ convert_ancil.sh
@@ -1,5 +1,6 @@
 cd outputs
-./../libs/conv2nc_ltol.tcl ../data/ancil_files/*.anc
+./../libs/conv2nc_ltol.tcl ../data/ancil_files/*
 ./../libs/conv2nc_ltol.tcl ../data/gc3_orca1_mask
 cd ..
 python addLatLon.py
+ncwa -a t outputs/qrparm.veg.nc -O outputs/qrparm.veg.nc
