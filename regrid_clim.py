038583592a5b9070ae208cf79f8d6d5dd3a8985c
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-06-03 11:58:00 +0100

    tidying and commenting

--- regrid_clim.py
+++ regrid_clim.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+###############################################
+## cfg                                       ##
+###############################################
 ## import libs
 import iris
 import matplotlib.pyplot as plt
@@ -13,50 +16,47 @@ root_dir = "data/"
 clim_dir = "half_deg_files/"
 outp_dir = "outputs/"
 
-show_plots = False
-
-## Load mask and remove coordinate system
+###############################################
+## Load mask and remove coordinate system    ##
+###############################################
 mask_file = root_dir + "gc3_orca1_mask"
 mask_list = iris.load( mask_file)
 mask = mask_list[0]
 
-print( "Original coord_system")
-print( mask.coord("latitude").coord_system)
-print( mask.coord("longitude").coord_system)
-
-#mask.coord("longitude").units="m"
-#mask.coord("latitude").units="m"
 mask.coord("latitude").coord_system=None
 mask.coord("longitude").coord_system=None
 
-print( "New coord_system")
-print( mask.coord("latitude").coord_system)
-print( mask.coord("longitude").coord_system)
-
+###############################################
+## Re-grid climate files and make drive_file ##
+###############################################
 drive_date = []
 fnames = []
 for input_file in listdir(root_dir + clim_dir):
-
+    if input_file == 'GSWP3.BC.SWdown.3hrMap.2010.N96e.nc': continue
+    
+    ## Cal filenames
     output_file = outp_dir + "regridded_" + input_file
     file = root_dir + clim_dir + input_file
     print(file)
    
-    cube = iris.load_cube( file)
-    
+    ## load cube
+    cube = iris.load_cube( file) 
     date = str(cube.coord('time')[1])[10:29]
+    
+    ## Find new drive_file entry
     if date not in drive_date: 
         drive_date.append(date)
         fname = '%vv'.join(output_file.split(cube.var_name))
         fnames.append(fname) 
 
+    ## Regrid and ouput climate
     output_data = cube.regrid( mask, iris.analysis.Nearest())
     iris.save(output_data, output_file)
 
-
+## Output drive_file
 drive_info = '\n'.join("'" + getcwd() + '/' + a + "', '" + b + "'" for a,b in zip(fnames, drive_date))
-
 drive_file = open("outputs/drive_file.txt", "w")
 drive_file.write(drive_info)
 drive_file.close()
 
-#pdb.set_trace()
+
